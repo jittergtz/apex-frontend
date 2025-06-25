0990b28e58fc037befb9f22bd606bbafb5ea89a0
jittergtz <sandro.gantze@icloud.com>
2025-06-25 15:18:33 +0200

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+
+  // Recommended: Disable image optimization API for static export
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
